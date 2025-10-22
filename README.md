ee7174dd0d06ec9bd623fc5192e6dba2e7414134
Aiden Lee <aidenlee@sas.upenn.edu>
2025-10-22 16:46:16 -0400

    implement basic extractor function, initialize frontend folder

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # vidtoscore
-extract scores from videos into a pdf
+
+extract scores from videos into a pdf.
+
+## Frontend
+
+- Allows user to input a youtube url for a video they want.
+- Move to an edit page, which displays the video like a video editor.
+  - Time parameters:
+    - User inputs manually the start and end of video they want to extract / can also have sliders
+    - User inputs or slides interval
+    - So start, end, and interval information provided
+  - Image parameters:
+    - Shows the video, and allow user to select the region to extract.
+- Extract the image section from each video frame according to start, end, interval
+- Maybe give user the option to resize the images, the space between each images, and how many images to put in one page.
+
+## Backend
+
+- First takes the url, downloads, then send to frontend to display in editing page.
+- For second part, take the time parameters and image section and return the list of images.
---
+++ extractor.py
@@ -0,0 +1,53 @@
+import cv2
+import subprocess
+import os
+from pathlib import Path
+import uuid
+
+
+class VideoDownloadError(Exception):
+    """Raised when a video download fails for any reason."""
+    pass
+
+
+def extract(vid_url, x1, y1, x2, y2, start, end, interval):
+    try:
+        download_dir = "downloads"
+        Path(download_dir).mkdir(exist_ok=True)
+
+        # Generate unique filename using UUID
+        unique_filename = f"{uuid.uuid4()}.mp4"
+        video_file_path = os.path.join(download_dir, unique_filename)
+        command = [
+            "yt-dlp",
+            "-f", "best",
+            "-o", video_file_path,
+            vid_url
+        ]
+        subprocess.run(command, capture_output=True, text=True, check=True)
+
+    except subprocess.CalledProcessError as e:
+        raise VideoDownloadError(
+            f"Error downloading video at {vid_url}: {e.stderr}")
+
+    result = []
+    video = cv2.VideoCapture(video_file_path)
+
+    for time in range(start, end, interval):
+        video.set(cv2.CAP_PROP_POS_MSEC, time)
+        success, img = video.read()
+        if success:
+            cropped_img = img[y1:y2, x1:x2]
+            result.append(cropped_img)
+
+    video.release()
+    return result
+
+
+res = extract("https://www.youtube.com/watch?v=vBDpwjn2SZ0",
+              0, 0, 800, 400, 8000, 180000, 15000)
+
+for i, img in enumerate(res):
+    cv2.imshow(f'Image {i}', img)
+    cv2.waitKey(1000)  # Display for 500ms
+    cv2.destroyAllWindows()
